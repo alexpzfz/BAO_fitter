f05c2b82bb6374ab77069894ba929536fcd3bd20
Alejandro Pérez <30901988+alexzpi@users.noreply.github.com>
2021-10-27 17:17:07 -0500

    Brand new truncate_convariance.py

---
+++ truncate_covariance.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def truncate(cov_path, q_min, q_max):
+    old_cov = np.loadtxt(cov_path, unpack=True)
+
+    # removing extra bins
+    mask_i = (q_min<=old_cov[0]) & (old_cov[0]<=q_max)
+    mask_j = (q_min<=old_cov[1]) & (old_cov[1]<=q_max)
+    mask = mask_i & mas_j
+    cov = old_cov[:, mask]
+
+    # removing hexadecapole
+    n_q = int(np.sqrt(len(cov[2]))/3)
+    cov_matrix_complete = cov[2]
+    cov_matrix_complete = np.reshape(cov_matrix_complete, (3*n_q, 3*n_q))
+    cov_matrix = cov_matrix_complete[:2*n_q, :2*n_q]
+
+    return cov_matrix
