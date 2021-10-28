90887d8a246036615201217af185029d500e77eb
Alejandro Pérez <30901988+alexzpi@users.noreply.github.com>
2021-10-27 22:18:43 -0500

    New implementation: arbitrary broad-band terms

--- best_fit.py
+++ best_fit.py
@@ -24,6 +24,8 @@ space = lines[7].split()[1]
 q_min = float(lines[8].split()[1]) # q means either s or k
 q_max = float(lines[9].split()[1])
 n_mu = int(lines[10].split()[1])
+bb_exp = list(map(int, lines[11].split(sep=': ')[1].split(sep=', ')))
+
 
 # Linear template and mu
 k, ps_lin = np.loadtxt(ps_lin_path, unpack=True)
@@ -34,14 +36,13 @@ mu = np.linspace(0.001, 1., n_mu)
 data = np.loadtxt(mul_path, usecols=(0,1,2), unpack=True)
 q_mask = (q_min<=data[0]) & (data[0]<=q_max)
 data = data[:, q_mask]
-n_q = len(data[0])
 
 # covariance matrix
 cov_matrix = truncate(cov_path, q_min, q_max)
 cov_inv = np.linalg.inv(cov_matrix)
 
 # minimise
-best_fit = op.minimize(chi2, [1, 0.35, 1, 1, 4, 2.5, 3], args=(data, cov_inv, [k, ps_lin], mu, sigma_r, iso, None, space), bounds=[(1, 10), (0, 5), (0.8, 1.2), (0.8, 1.2), (0, 12), (0, 12), (0, 8)])
+best_fit = op.minimize(chi2, [1, 0.35, 1, 1, 4, 2.5, 3], args=(data, cov_inv, [k, ps_lin], mu, sigma_r, iso, bb_exp, None, space), bounds=[(1, 10), (0, 5), (0.8, 1.2), (0.8, 1.2), (0, 12), (0, 12), (0, 8)])
 
 # get broad band coefficients
 b, beta, alpha_par, alpha_per, S_par, S_per, S_s = best_fit.x
@@ -52,7 +53,7 @@ if space=='config':
 elif space=='fourier':
     model = ps_multipoles_template(k_data, k_lin, mu, ps_lin, S_par, S_per, S_s, sigma_r, b, beta, iso, alpha_par, alpha_per)
 
-coeffs = broadband(data, model, cov_inv, space=space)[2]
+coeffs = broadband(data, model, cov_inv, bb_exp)[2]
 
 # save as a dictionary
 data_dict = {}
@@ -63,6 +64,7 @@ data_dict['Sigma_r'] = sigma_r
 data_dict['iso'] = iso
 data_dict['best_fit_out'] = best_fit
 data_dict['best_fit_values'] = {'b':b, 'beta':beta, 'alpha_par':alpha_par, 'alpha_per':alpha_per, 'Sigma_par':S_par, 'Sigma_per':S_per, 'Sigma_fog':S_s}
-data_dict['broad_band'] = coeffs
+data_dict['broad_band_exp'] = bb_exp
+ddat_dict['broad_band_coeff'] = np.split(coeffs, 2)
 with open(out_path + label.lower() + '.dict', 'wb') as file:
     pickle.dump(data_dict, file)
--- chi_squared.py
+++ chi_squared.py
@@ -4,30 +4,18 @@ import scipy.integrate as integra
 from ps_template import *
 from multipoles import *
 
-def broadband(data, model, cov_inv, space='config'):
-    if space=='config':
-        s, xi0, xi2 = data
-        xi0m, xi2m = model
-        V = np.concatenate((xi0, xi2)) - np.concatenate((xi0m, xi2m))
+def broadband(data, model, cov_inv, exp_list):
+    q, f0, f2 = data  # q can be either s or k, while f can be either xi or ps
+    f0m, f2m = model
+    V = np.concatenate((f0, f2)) - np.concatenate((f0m, f2m))
 
-        # broad band terms
-        A = np.zeros((6, 2*len(s)))
-        for i in range(0, 3):
-            A[i, 0:len(s)] = s**(i - 2)
-            A[3+i, len(s):] = s**(i - 2)
-
-    elif space=='fourier':
-        k, ps0, ps2 = data
-        ps0m, ps2m = model
-        V = np.concatenate((ps0, ps2)) - np.concatenate((ps0m, ps2m))
-
-        # broad band terms
-        A = np.zeros((10, 2*len(k)))
-        for i in range(0, 4):
-            A[i, 0:len(k)] = k**(i - 3)
-            A[5+i, len(k):] = k**(i - 3)
-        A[4, 0:len(k)] = k**2
-        A[9, len(k):] = k**2
+    # broad-band terms
+    nexp = len(exp_list)
+    nq = len(q)
+    A = np.zeros((2*nexp, 2*nq))
+    for i in range(0, nexp):
+        A[i, :nq] = q**exp_list[i]
+        A[nexp+i, nq:] = q**exp_list[i]
 
     # Analytical marginalisation
     alpha_ = A @ cov_inv @ A.T
@@ -38,7 +26,7 @@ def broadband(data, model, cov_inv, space='config'):
     return bb0, bb2, coeffs
 
 
-def chi2(theta, data, cov_inv, linear_template, mu , S_r, iso, sigmas=None, space='config'):
+def chi2(theta, data, cov_inv, linear_template, mu , S_r, iso, bb_exp, sigmas=None, space='config'):
     if sigmas==None: # for best-fit
         b, beta, alpha_par, alpha_per, S_par, S_per, S_s = theta
     else: # for mcmc with fixed sigmas
@@ -52,7 +40,7 @@ def chi2(theta, data, cov_inv, linear_template, mu , S_r, iso, sigmas=None, spac
         pt = power_spectrum_template(k, mu, pk_lin, S_par, S_per, S_s, S_r, b, beta, iso)
         xi0m, xi2m = two_point_cf_template(s, k, mu, pt, alpha_par, alpha_per)
         V = np.concatenate((xi0m, xi2m)) - np.concatenate((xi0, xi2))
-        bb0, bb2, _ = broadband([s, xi0, xi2], [xi0m, xi2m], cov_inv, space='config')
+        bb0, bb2, _ = broadband([s, xi0, xi2], [xi0m, xi2m], cov_inv, bb_exp)
 
     elif space=='fourier':
         k_data, ps0, ps2 = data
@@ -60,7 +48,7 @@ def chi2(theta, data, cov_inv, linear_template, mu , S_r, iso, sigmas=None, spac
 
         ps0m, ps2m = ps_multipoles_template(k_data, k_lin, mu, pk_lin, S_par, S_per, S_s, S_r, b, beta, iso, alpha_par, alpha_per)
         V = np.concatenate((ps0m, ps2m)) - np.concatenate((ps0, ps2))
-        bb0, bb2, _ = broadband([k_data, ps0, ps2], [ps0m, ps2m], cov_inv, space='fourier')
+        bb0, bb2, _ = broadband([k_data, ps0, ps2], [ps0m, ps2m], cov_inv, bb_exp)
 
     Vn = V + np.concatenate((bb0, bb2))
     chi2 = Vn.T @ cov_inv @ Vn
@@ -85,9 +73,9 @@ def logprior(theta, sigmas=None):
         lp += 0. if 0 < S_s < 8 else -np.inf
     return lp
 
-def loglike(theta, data, cov_inv, linear_template, S_r, iso, sigmas=None, space='config'):
-    return -0.5 * chi2(theta, data, cov_inv, linear_template, S_r, iso, sigmas, space)
+def loglike(theta, data, cov_inv, linear_template, S_r, iso, bb_exp, sigmas=None, space='config'):
+    return -0.5 * chi2(theta, data, cov_inv, linear_template, S_r, iso, bb_exp, sigmas, space)
 
-def logpost(theta, data, cov_inv, linear_template, S_r, iso, sigmas=None, space='config'):
+def logpost(theta, data, cov_inv, linear_template, S_r, iso, bb_exp, sigmas=None, space='config'):
     '''The natural logarithm of the posterior.'''
-    return logprior(theta, sigmas) + loglike(theta, data, cov_inv, linear_template, S_r, iso, sigmas, space)
+    return logprior(theta, sigmas) + loglike(theta, data, cov_inv, linear_template, S_r, iso, bb_exp, sigmas, space)
